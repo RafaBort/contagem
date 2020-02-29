46642d02dfe1bcf483a929d0b01151c6a6dc6bbd
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-02-28 22:15:39 -0300

    realizei uma contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace contagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Um...");
+            Console.WriteLine("Dois...");
+            Console.WriteLine("Três...");
+            Console.WriteLine("EEE...");
+            Console.WriteLine("JÁÁÁÁÁÁ!!!!");
+        }
+    }
+}
